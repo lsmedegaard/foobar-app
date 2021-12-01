059584794221bf7af9b7971da23f4b4295171f00
lasse <lass948m@stud.kea.dk>
2021-12-01 22:46:43 +0100

    burger menu

--- foobar_form_app/pages/main.js
+++ foobar_form_app/pages/main.js
@@ -1,9 +1,41 @@
 import "../../scss/style.scss";
 
-const logo = document.querySelector(".logo, .button_wrapper");
-const button = document.querySelector(".button_wrapper");
+// const logo = document.querySelector(".logo, .button_wrapper");
+// const button = document.querySelector(".button_wrapper");
 
-document.querySelector(".order_button").addEventListener("click", () => logo.classList.add("fadeUp"));
-document.querySelector(".order_button").addEventListener("click", () => button.classList.add("fadeUp"));
+// document.querySelector(".order_button").addEventListener("click", () => {
+//   logo.classList.add("fadeUp");
+//   button.classList.add("fadeUp");
+// });
 
-logo.addEventListener("animationend", () => (window.location.href = "beer_form.html"));
+// logo.addEventListener("animationend", () => {
+//   window.location.href = "beer_form.html";
+// });
+
+const menuBtn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    hamburger.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
+    navItems.forEach((item) => item.classList.add("open"));
+
+    showMenu = true;
+  } else {
+    hamburger.classList.remove("open");
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
+    navItems.forEach((item) => item.classList.remove("open"));
+
+    showMenu = false;
+  }
+}
