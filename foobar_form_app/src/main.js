066a323f33dedb79e44d3c64b56ef53911089ed2
Johan <joha034p@stud.kea.dk>
2021-12-10 14:48:38 +0100

    help page, styling and dynamic products

--- foobar_form_app/src/main.js
+++ foobar_form_app/src/main.js
@@ -31,3 +31,105 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+handleBeers();
+
+//create array of beers with properies
+//display each beer - "displayBeer(beer)"
+function handleBeers() {
+  const beers = [
+    {
+      name: "Row 26",
+      price: 35,
+      img: "/assets/images/row26.png",
+      type: "Stout",
+    },
+
+    {
+      name: "Ruined childhood",
+      price: 40,
+      img: "/assets/images/ruinedchildhood.png",
+      type: "Porter",
+    },
+
+    {
+      name: "Fairytale Ale",
+      price: 35,
+      img: "/assets/images/fairytaleale.png",
+      type: "Ale",
+    },
+    {
+      name: "Githop",
+      price: 40,
+      img: "/assets/images/githop.png",
+      type: "Ale",
+    },
+    {
+      name: "Hoppily Ever After",
+      price: 50,
+      img: "/assets/images/hoppilyeverafter.png",
+      type: "Belgian IPA",
+    },
+    {
+      name: "Mowintime",
+      price: 35,
+      img: "/assets/images/mowintime.png",
+      type: "Pilsner",
+    },
+    {
+      name: "El Hefe",
+      price: 35,
+      img: "/assets/images/elhefe.png",
+      type: "Wheat beer",
+    },
+    {
+      name: "Hollaback",
+      price: 40,
+      img: "/assets/images/hollaback.png",
+      type: "Lager",
+    },
+    {
+      name: "Sleighride",
+      price: 40,
+      img: "/assets/images/sleighride.png",
+      type: "Porter",
+    },
+    {
+      name: "Steampunk",
+      price: 50,
+      img: "./assets/images/steampunk.png",
+      type: "IPA",
+    },
+  ];
+
+  beers.forEach((beer) => {
+    displayBeer(beer);
+  });
+}
+
+function displayBeer(beer) {
+  // create clone
+  const clone = document.querySelector("template").content.cloneNode(true);
+
+  // set clone data
+  clone.querySelector("img").src = beer.img;
+  clone.querySelector(".beer_name").textContent = beer.name;
+  clone.querySelector(".beer_type").textContent = beer.type;
+  clone.querySelector(".price").textContent = beer.price + "kr";
+  clone.querySelector("button").addEventListener("click", addToCart);
+
+  // append clone to list
+  document.querySelector(".beers_container ul").appendChild(clone);
+}
+
+const cart = [];
+
+function addToCart() {
+  console.log("adding to cart");
+  cart.push(this);
+  console.log(cart);
+}
+
+function removeFromCart() {}
+
+function displayCart() {}
