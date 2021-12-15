fac7e92953483065628ac2af211b653ceb39d50f
Lasse <lass948m@stud.kea.dk>
2021-12-15 20:23:56 +0100

    bugfixes

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
+import { resolve } from 'path'
+
 export default {
   root: "src",
+  build: {
+    rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'src/index.html'),
+            cart: resolve(__dirname, 'src/cart.html'),
+            help: resolve(__dirname, 'src/help.html'),
+            pay: resolve(__dirname, 'src/pay.html'),
+            payment_accepted: resolve(__dirname, 'src/payment_accepted.html')
+        }
+    }
+}
 };
