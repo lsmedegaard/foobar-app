d1f0637be92fcc756cecb58a41c6730eb82973ad
lasse <lass948m@stud.kea.dk>
2021-12-16 10:38:30 +0100

    minor bug changes

--- src/main.js
+++ src/main.js
@@ -65,6 +65,7 @@ function displayBeer(beer) {
   clone.querySelector(".beer_type").textContent = beer.type;
   thePrice.textContent = beer.price + "kr";
   const quantity = clone.querySelector(".quantity");
+
   clone.querySelector("#remove").addEventListener("click", () => {
     if (cart[beer.name] === 0) cart[beer.name] = 0;
     else {
@@ -84,7 +85,7 @@ function displayBeer(beer) {
     localStorage.setItem("cart", JSON.stringify(cart));
     totalSum.textContent = total + " " + "kr" + " " + "pay";
   });
-
+  totalSum.textContent = total + " " + "kr" + " " + "pay";
   // append clone to list
   document.querySelector(".beers_container ul").appendChild(clone);
 }
