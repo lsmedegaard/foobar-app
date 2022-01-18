5c4712e26b2117e367f2b757c83168e06dbd51e4
Lasse <lass948m@stud.kea.dk>
2022-01-18 16:33:52 +0100

    sidste rettelser

--- src/main.js
+++ src/main.js
@@ -85,6 +85,10 @@ function displayBeer(beer) {
   clone.querySelector(".beer_type").textContent = beer.type;
   thePrice.textContent = beer.price + "kr";
   const quantity = clone.querySelector(".quantity");
+  quantity.textContent = " " + "x" + cart[beer.name];
+  quantity.appendChild(document.createElement('div'))
+
+
 
   clone.querySelector(".remove").addEventListener("click", () => {
     if (cart[beer.name] === 0) cart[beer.name] = 0;
